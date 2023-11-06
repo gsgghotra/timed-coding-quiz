76ea404d60543e3b1a5feccad1396b51b8592160
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-06 11:38:39 +0000

    First question added. Function structure done

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,20 @@
 // Event listener that listens to the start quiz button
 let startButton = document.getElementById('start');
 startButton.addEventListener("click", startQuiz);
+
 // When start quiz is clicked, hide start screen, show question screen
 let startScreen = document.getElementById('start-screen');
 let questionsScreen = document.getElementById('questions');
 let endScreen = document.getElementById('end-screen');
+
 //Each question can be saved in array of objects
+questions = [
+    {
+        question: "Who is the best player of all time?",
+        answers: ['Ronaldo', 'Messi', 'Zidane', 'Ronaldinho'],
+        correct_answer : 'Ronaldo'
+    }
+]
 
 //functions are listed below
 function startQuiz(event){
@@ -14,4 +23,14 @@ function startQuiz(event){
     startScreen.classList.add('hide');
     //Make questions Screen Visible
     questionsScreen.classList.remove('hide');
+    askQuestions()
+}
+
+function askQuestions(){
+    //get question-title element
+    let questionTitle = document.getElementById('question-title');
+    let questionChoices = document.getElementById('choices');
+
+    //ask questions
+    questionTitle.innerText = questions[0].question; 
 }
---
+++ assets/js/questions.js
@@ -0,0 +1,7 @@
+questions = [
+    {
+        question: "Who is the best player of all time?",
+        answers: ['Ronaldo', 'Messi', 'Zidane', 'Ronaldinho'],
+        correct_answer : 'Ronaldo'
+    }
+]
