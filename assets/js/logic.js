29cd083dd482086104767a6838e45e84c84bffbe
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-06 17:56:05 +0000

    score function added. end screen after last question

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,6 +7,10 @@ let startScreen = document.getElementById('start-screen');
 let questionsScreen = document.getElementById('questions');
 let endScreen = document.getElementById('end-screen');
 
+//get question-title and choices element 
+let questionTitle = document.getElementById('question-title');
+let questionChoices = document.getElementById('choices');
+
 //feedback section
 let feedback =  document.createElement('p');
 
@@ -24,17 +28,12 @@ function startQuiz(event){
 }
 
 function askQuestions(questionNumber){
-    //get question-title and choices element 
-    let questionTitle = document.getElementById('question-title');
-    let questionChoices = document.getElementById('choices');
-
-    //Empty the choices
+    //Empty the choices prior to question
     questionChoices.innerText = '';
-
     //ask questions
     questionTitle.innerText = questions[questionNumber].question; 
 
-    //create ol
+    //display options for the given question
     for(var i = 1 ; i < 5 ; i++){
         //created 4 variables for buttons (option1, option2, option3, option4)
         var currentOption = `option${i}`;
@@ -45,24 +44,26 @@ function askQuestions(questionNumber){
         //Add to the DOM
         questionChoices.appendChild(currentOption);
     }
-
+    //this function checks if chosen answer is correct
     userAnswer(questionChoices, questionNumber);
 }
 
 //Check which option the user chooses
 function userAnswer(questionChoices, questionNumber){
-        let chosenAnswer;
-        //Listen for the user answer
-        questionChoices.addEventListener('click', function(event) {
-            if (event.target.tagName.toLowerCase() === 'button') {
-                chosenAnswer = event.target.innerText
-                checkAnswer(chosenAnswer, questionNumber);
-                //console.log('Button Clicked ' + event.target.innerText);
-            }
-        });
+    let chosenAnswer;
+    //Listen for the user answer
+    questionChoices.addEventListener('click', function(event) {
+        if (event.target.tagName.toLowerCase() === 'button') {
+            chosenAnswer = event.target.innerText
+            checkAnswer(chosenAnswer, questionNumber);
+        }
+    });
+
 }
 
 function checkAnswer(chosenAnswer, questionNumber){
+
+    //Check Answer
     //First 3 characters have been removed as they include the option number in innerText
     if (chosenAnswer.slice(3) === questions[questionNumber].correct_answer){
         //Correct Answer
@@ -72,11 +73,30 @@ function checkAnswer(chosenAnswer, questionNumber){
     }
     feedback.classList.add('feedback')
     questionsScreen.append(feedback);
-    //hide feedback after 1 seconds
+
+    //hide feedback after 1 seconds using set timeout
     setTimeout(()=>{
         feedback.remove();
     },1000);
 
-    //Next Question
-    askQuestions(questionNumber+1);
+    //Save the scores
+    processScore(questionNumber)
+   
+}
+
+function processScore(questionNumber) {
+    //Calculate and save score
+
+    //Check if more questions are left
+    if (questionNumber >= 2){
+        //Display end Screen
+        //Hide the start Screen
+        questionsScreen.classList.add('hide');
+        //Make questions Screen Visible
+        endScreen.classList.remove('hide');
+    } else {
+        //ask next question
+        askQuestions(questionNumber+1);
+    }
+    
 }
