3e26524243955a0a16f83453b049388637a9da0a
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-06 10:35:39 +0000

    Start Quiz event listener added

---
+++ assets/js/logic.js
@@ -0,0 +1,11 @@
+// Event listener that listens to the start quiz button
+let startButton = document.getElementById('start');
+startButton.addEventListener("click", startQuiz);
+// When start quiz is clicked, hide start screen, show question screen
+
+//Each question can be saved in array of objects
+
+//functions are listed below
+function startQuiz(event){
+    console.log("Start button clicked");
+}
--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-// Event listener that listens to the start quiz button
-
-// When start quiz is clicked, hide start screen, show question screen
-
-//Each question can be saved in array of objects
-
