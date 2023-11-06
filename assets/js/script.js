93154ac0fd374e0e4637ba3d05ccb8271846f5e8
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-06 09:59:44 +0000

    JS file added, comments include planning draft

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Event listener that listens to the start quiz button
+
+// When start quiz is clicked, hide start screen, show question screen
+
+//Each question can be saved in array of objects
+
