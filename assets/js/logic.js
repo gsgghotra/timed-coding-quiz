a7307f47355cf23ae310d8a618bb69259b7a806d
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-06 11:25:25 +0000

    Start Button working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,10 +2,16 @@
 let startButton = document.getElementById('start');
 startButton.addEventListener("click", startQuiz);
 // When start quiz is clicked, hide start screen, show question screen
-
+let startScreen = document.getElementById('start-screen');
+let questionsScreen = document.getElementById('questions');
+let endScreen = document.getElementById('end-screen');
 //Each question can be saved in array of objects
 
 //functions are listed below
 function startQuiz(event){
     console.log("Start button clicked");
+    //Hide the start Screen
+    startScreen.classList.add('hide');
+    //Make questions Screen Visible
+    questionsScreen.classList.remove('hide');
 }
